a808fc2d7026f8b5c92ce265bc39393f05939c37
mengruwu <ray7102ray7102>
2016-10-13 18:36:46 +0800

    PTT

--- index.js
+++ index.js
@@ -20,44 +20,87 @@ app.get('/', function(request, response) {
 });
 
 var urls = [];
+var newesturl;
+
 var io = socket_io.listen(httpServer);
 io.sockets.on('connection', function(socket){
 	socket.on('newpage',function(data){
 		//console.log(data.pages);
-		request('http://ck101.com/forum-'+data.url+'-'+data.pages+'.html', function (err, res, body) {
-			if (err) throw new Error(err);
-			var $ = cheerio.load(body);
-			$('a.s.xst').each(function () {
-				var url = $(this).attr('href');
-				urls.push(url);
-			});
-			//console.log('['+urls.length+']');
-			for (var i = 0; i < urls.length; i++) {
-				var url = urls[i];
-				(function(url){
-					request(url, function (err, res, body) {
-						if (err) throw new Error(err);
-						var $ = cheerio.load(body);
-						var imgurls = [];
-						$('.mbn img').each(function (index, element) {
-							var imgurl = $(this).attr('file');
-							if (imgurl!=null)imgurls.push(imgurl);
-							if (index > 7) {return false;}
-						});
-						if (imgurls.length==0) {
-							$('td.t_f img').each(function (index, element) {
+		if ( data.sorts == 'ck101' ) {
+			request('http://ck101.com/forum-'+data.url+'-'+data.pages+'.html', function (err, res, body) {
+				if (err) throw new Error(err);
+				var $ = cheerio.load(body);
+				$('a.s.xst').each(function () {
+					var url = $(this).attr('href');
+					urls.push(url);
+				});
+				//console.log('['+urls.length+']');
+				for (var i = 0; i < urls.length; i++) {
+					var url = urls[i];
+					(function(url){
+						request(url, function (err, res, body) {
+							if (err) throw new Error(err);
+							var $ = cheerio.load(body);
+							var imgurls = [];
+							$('.mbn img').each(function (index, element) {
+								if (index >= 5) {return false;}
 								var imgurl = $(this).attr('file');
-								if (imgurl != null)imgurls.push(imgurl);
-								if (index > 7) {return false;}
+								if (imgurl!=null)imgurls.push(imgurl);
 							});
-						}
-						if(imgurls.length >= 3)socket.emit('putImg',{'url' : url , 'src':imgurls});
-						imgurls = [];
-					});	
-				})(url);
-			}
-			urls = [];
-		});	  
+							if (imgurls.length==0) {
+								$('td.t_f img').each(function (index, element) {
+									if (index >= 5) {return false;}
+									var imgurl = $(this).attr('file');
+									if (imgurl != null)imgurls.push(imgurl);
+								});
+							}
+							if(imgurls.length >= 3)socket.emit('putImg',{'url' : url , 'src':imgurls});
+							imgurls = [];
+						});	
+					})(url);
+				}
+				urls = [];
+			});	
+		}
+		else if ( data.sorts == 'ptt' ) {
+			var ptturl;
+			if(data.pages == 1)ptturl = 'https://www.ptt.cc/bbs/Beauty/index.html';
+			else ptturl = newesturl;
+			request(ptturl, function (err, res, body) {
+				if (err) throw new Error(err);
+				var $ = cheerio.load(body);
+				$('div.btn-group-paging a').each(function (index, element) {
+					var url = $(this).attr('href');
+					if(index == 1) newesturl = 'https://www.ptt.cc'+url;
+				});
+
+				$('div.title a').each(function () {
+					var url = $(this).attr('href');
+					urls.push(url);
+				});
+
+				for (var i = 0; i < urls.length; i++)  {
+					var url = 'https://www.ptt.cc'+urls[i];
+					(function(url){
+						request(url, function (err, res, body) {
+							if (err) throw new Error(err);
+							var $ = cheerio.load(body);
+							var imgurls = [];
+							$('div.richcontent img').each(function (index, element) {
+								if (index >= 5) {return false;}
+								var imgurl = $(this).attr('src');
+								if (imgurl != null)imgurls.push(imgurl);
+							});
+							if(imgurls.length >= 2){
+								socket.emit('putImg',{'url' : url , 'src':imgurls});
+							}
+							imgurls = [];
+						});	
+					})(url);
+				}
+				urls = [];
+			});
+		}  
 	});		
 });
 
